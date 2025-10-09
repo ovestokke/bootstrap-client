38d73a5ad6955c50c9a6fae8c7effc0e779380f3
Ove Stokke <14012923+ovestokke@users.noreply.github.com>
2025-10-09 14:07:02 +0200

    Add WezTerm configuration with custom coolnight theme and keybindings

---
+++ wezterm.lua
@@ -0,0 +1,54 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- config.color_scheme = 'Lunaria Dark (Gogh)'
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 16
+
+config.enable_tab_bar = true
+
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 10
+
+-- my coolnight colorscheme:
+config.colors = {
+	foreground = "#CBE0F0",
+	background = "#011423",
+	cursor_bg = "#47FF9C",
+	cursor_border = "#47FF9C",
+	cursor_fg = "#011423",
+	selection_bg = "#033259",
+	selection_fg = "#CBE0F0",
+	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
+	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+}
+
+config.keys = {
+  -- Turn off the default CMD-m Hide action, allowing CMD-m to
+  -- be potentially recognized and handled by the tab
+  {
+    key = 'd',
+    mods = 'CMD',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+    {
+    key = 'd',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+}
+-- and finally, return the configuration to wezterm
+return config
